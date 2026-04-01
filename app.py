59f241030ef9bd061a4387bb830b24373739acaa
MrAdam101 <adamconroy90@gmail.com>
2026-04-01 14:59:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from datetime import date, timedelta
 
+from utils.logic import get_teaching_dates, split_remaining_pages
+
 def load_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
